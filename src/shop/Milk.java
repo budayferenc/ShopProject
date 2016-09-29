9e5713581d6d31ed60bb36668dd1d9c37d80165c
Ferenc Buday <budayferenc@gmail.com>
2016-09-29 06:54:50 +0200

    task 1.1

---
+++ src/shop/Milk.java
@@ -0,0 +1,49 @@
+package shop;
+
+import java.time.LocalDateTime;
+
+public class Milk {
+	protected int capacity;
+	protected String manufacturer;
+	protected LocalDateTime warranty;
+	protected double fat;
+	protected long price;
+	
+	public Milk(int capacity, String manufacturer, LocalDateTime warranty, double fat, long price) {
+		this.capacity = capacity;
+		this.manufacturer = manufacturer;
+		this.warranty = warranty;
+		this.fat = fat;
+		this.price = price;
+	}
+	
+	public boolean isGood(){
+		return LocalDateTime.now().isBefore(warranty);
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public LocalDateTime getWarranty() {
+		return warranty;
+	}
+
+	public double getFat() {
+		return fat;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	@Override
+	public String toString() {
+		return "Milk [capacity=" + capacity + ", manufacturer=" + manufacturer + ", warranty=" + warranty + ", fat="
+				+ fat + ", price=" + price + "]";
+	}
+}
